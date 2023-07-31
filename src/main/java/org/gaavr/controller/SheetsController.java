d9b8aa4feb9b98c2a59e5e533bb17ef270497d0d
Andrei A <gavrjob@gmail.com>
2023-07-31 21:36:43 +0300

    got data from table

--- src/main/java/org/gaavr/controller/SheetsController.java
+++ src/main/java/org/gaavr/controller/SheetsController.java
@@ -1,35 +1,31 @@
 package org.gaavr.controller;
 
-import lombok.RequiredArgsConstructor;
 import org.gaavr.config.GoogleConfig;
 import org.gaavr.google.GoogleSheetsReader;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.security.GeneralSecurityException;
 import java.util.List;
 
 @RestController
 @RequestMapping("/sheets")
-@RequiredArgsConstructor
 public class SheetsController {
 
     private final GoogleSheetsReader googleSheetsReader;
     private final GoogleConfig googleConfig;
 
-    @GetMapping("/read")
-    public void readDataFromSheets() {
-        String spreadsheetId = googleConfig.getSpreadsheetId();
-        String range = "Sheet1!A1:C";
-        List<List<String>> data = googleSheetsReader.readData(spreadsheetId, range);
-
-        // Выводим данные в консоль
-        for (List<String> row : data) {
-            for (String cell : row) {
-                System.out.print(cell + "\t");
-            }
-            System.out.println();
-        }
+    public SheetsController(GoogleSheetsReader googleSheetsReader, GoogleConfig googleConfig) {
+        this.googleSheetsReader = googleSheetsReader;
+        this.googleConfig = googleConfig;
     }
 
+    @GetMapping("/data")
+    public List<List<Object>> getDataFromSheets() throws IOException, GeneralSecurityException {
+        String spreadsheetId = googleConfig.getSpreadsheetId();
+        String range = "УБВТ21!A478:B478"; // Замените на нужный диапазон данных
+        return googleSheetsReader.readData(spreadsheetId, range);
+    }
 }
--- src/main/java/org/gaavr/google/GoogleSheetsReader.java
+++ src/main/java/org/gaavr/google/GoogleSheetsReader.java
@@ -1,56 +1,29 @@
 package org.gaavr.google;
 
+import com.google.api.services.sheets.v4.Sheets;
 import com.google.api.services.sheets.v4.model.ValueRange;
-import lombok.AllArgsConstructor;
-import org.gaavr.config.GoogleConfig;
 import org.springframework.stereotype.Component;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
-import com.google.api.services.sheets.v4.Sheets;
-
-
 @Component
-@AllArgsConstructor
 public class GoogleSheetsReader {
 
-    private final GoogleConfig googleConfig;
     private final GoogleAuthorizeUtil googleAuthorizeUtil;
 
-    public List<List<String>> readData(String spreadsheetId, String range) {
-        try {
-            Sheets sheetsService = googleAuthorizeUtil.getSheetsService();
-
-            ValueRange response = sheetsService.spreadsheets().values()
-                    .get(spreadsheetId, range)
-                    .execute();
-
-            List<List<Object>> values = response.getValues();
-            List<List<String>> result = new ArrayList<>();
-
-            if (values != null) {
-                for (List<Object> row : values) {
-                    List<String> stringRow = new ArrayList<>();
-                    for (Object cell : row) {
-                        stringRow.add(String.valueOf(cell));
-                    }
-                    result.add(stringRow);
-                }
-            }
-
-            return result;
-        } catch (IOException | GeneralSecurityException e) {
-            e.printStackTrace();
-            System.out.println("readData упал");
-            return null;
-        }
+    public GoogleSheetsReader(GoogleAuthorizeUtil googleAuthorizeUtil) {
+        this.googleAuthorizeUtil = googleAuthorizeUtil;
     }
 
+    public List<List<Object>> readData(String spreadsheetId, String range) throws IOException, GeneralSecurityException {
+        Sheets sheetsService = googleAuthorizeUtil.getSheetsService();
 
+        ValueRange response = sheetsService.spreadsheets().values()
+                .get(spreadsheetId, range)
+                .execute();
+
+        return response.getValues();
+    }
 }
