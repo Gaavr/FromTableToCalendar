89d9c2dafeebba184eecb3a136607559961c7189
Andrei A <gavrjob@gmail.com>
2023-08-01 23:36:00 +0300

    calendar working

--- src/main/java/org/gaavr/service/GoogleCalendarService.java
+++ src/main/java/org/gaavr/service/GoogleCalendarService.java
@@ -1,13 +1,11 @@
 package org.gaavr.service;
 
-import com.google.api.client.util.DateTime;
 import com.google.api.services.calendar.Calendar;
 import com.google.api.services.calendar.model.Event;
 import com.google.api.services.calendar.model.Events;
 import lombok.RequiredArgsConstructor;
 import org.gaavr.config.GoogleConfig;
 import org.gaavr.util.GoogleAuthorizeUtil;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -27,10 +25,7 @@ public class GoogleCalendarService {
         try {
             Calendar calendarService = googleAuthorizeUtil.getCalendarService();
 
-            DateTime now = new DateTime(System.currentTimeMillis());
             Events events = calendarService.events().list(googleConfig.getCalendarId())
-                    .setMaxResults(10)
-                    .setTimeMin(now)
                     .setOrderBy("startTime")
                     .setSingleEvents(true)
                     .execute();
--- src/main/java/org/gaavr/util/GoogleAuthorizeUtil.java
+++ src/main/java/org/gaavr/util/GoogleAuthorizeUtil.java
@@ -26,7 +26,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.security.GeneralSecurityException;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 @Component
